5ad8edfd3007807d0b364b6f2dc52468c0b5aabd
Gligan Horea <horea.gligan@evozon.com>
2012-03-09 14:49:05 +0200

    Created project

---
+++ lib/File/RoundRobin.pm
@@ -0,0 +1,111 @@
+package File::RoundRobin;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+File::RoundRobin - The great new File::RoundRobin!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use File::RoundRobin;
+
+    my $foo = File::RoundRobin->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Gligan Calin Horea, C<< <gliganh at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-file-roundrobin at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=File-RoundRobin>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc File::RoundRobin
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=File-RoundRobin>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/File-RoundRobin>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/File-RoundRobin>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/File-RoundRobin/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Gligan Calin Horea.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of File::RoundRobin
